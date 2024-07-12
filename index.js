169a830a9764983d7f1dcf1251cae7b4014c6c82
Deepak-rock <deepakind777@gmail.com>
2024-07-12 13:59:05 +0530

    second commit

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 const element = (
-    // Write your code here.
-  )
-  
+  <div className='bg-img-container'>
+    <h1 className='heading'>Congratulations</h1>
+    <div className='bg-container'>
+      <img
+          src='https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png'
+          className='profile'
+      />
+      <h2 className='profile-name'>Kiran V</h2>
+      <p className='para'>
+        Vishnu institute of Computer Education and Technology, Bhimavaram</p>
+      <img
+        src='https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png'
+        className='watch'
+      />
+    </div>
+  </div>
+)
 ReactDOM.render(element, document.getElementById('root'))
